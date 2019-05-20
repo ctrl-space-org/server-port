fb94c3404ed3e8a3e98d84d49b3f8cc0334e251d
Leonardo Lucas Dos Santos <sombratwo@localhost.localdomain>
2019-05-19 23:37:59 -0300

    adicionado atributo de plugins para server

--- src/Ports/Plugins.ts
+++ src/Ports/Plugins.ts
@@ -1,4 +1,4 @@
 export interface Plugins {
     json():any
-    multiPart():Function
+    multiPart(): any
 };
--- src/Ports/Server.ts
+++ src/Ports/Server.ts
@@ -1,4 +1,6 @@
 import { Router } from './Router'
+import { Plugins } from './Plugins'
 export interface Server extends Router{
+    plugins: Plugins
     listen(port?: string, fun?: Function):void
 }
