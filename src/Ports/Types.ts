4710d4339e4efecc17780b4258cd4e026ffbdd61
Leonardo Lucas dos Santos <leolucassang@gmail.com>
2019-06-09 16:01:45 +0000

    Adicionado types para server-port

---
+++ src/Ports/Types.ts
@@ -0,0 +1,3 @@
+
+const [ServerType, PluginsType, RouterType] = [Symbol('Server'), Symbol('Plugins'), Symbol('Router')]
+export { ServerType, PluginsType, RouterType }
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import ResponseInterface from './Ports/Response'
 import { Router } from './Ports/Router'
 import { Server } from './Ports/Server'
 import { Plugins } from './Ports/Plugins'
-
-export { ResponseInterface as Response, Request, Router, Server, Plugins }
+import { ServerType, PluginsType, RouterType } from "./Ports/Types";
+export { ServerType, PluginsType, RouterType, ResponseInterface as Response, Request, Router, Server, Plugins }
